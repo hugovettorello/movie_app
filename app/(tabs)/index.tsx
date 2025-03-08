5e47b1cd8c7200a3de6eb7818bceb76ad34eecae
hugovettorello <hugovettorello13@gmail.com>
2025-03-08 11:32:45 -0300

    feat: add route pages

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import React from "react";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
+        <Link href="/onboarding">Onboarding</Link>
+        <Link href="/movies/avengers">Avenger Movie</Link>
     </View>
   );
 }
---
+++ app/movies/[id].tsx
@@ -0,0 +1,13 @@
+import {View, Text, StyleSheet} from "react-native";
+import React from "react";
+
+const MovieDetails = () => {
+
+    return (
+        <View>
+            <Text>Movie Details </Text>
+        </View>
+    )
+}
+
+export default MovieDetails;
