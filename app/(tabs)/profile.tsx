585ecda80a9aa2678af88631023463c269f55b17
hugovettorello <hugovettorello13@gmail.com>
2025-03-09 12:04:15 -0300

    feat: add movie details page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,23 @@
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet,Image} from "react-native";
 import React from "react";
+import { icons } from "@/constants/icons";
 
 const Profile = () => {
     return (
-        <View>
-            <Text>Profile</Text>
+        <View
+            className="bg-primary flex-1 px-10"
+        >
+            <View
+                className="flex justify-center items-center flex-1 flex-col gap-5"
+            >
+                <Image source={icons.person}
+                    className="size-10"
+                    tintColor="#fff"
+                />
+                <Text
+                    className="text-gray-500 text-base"
+                >Profile</Text>
+            </View>
         </View>
     )
 }
